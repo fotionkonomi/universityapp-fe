a61da042904204ab13c6c0e64f9f26c3ad4415cd
Fotion.Konomi@fti.edu.al <Fotion.Konomi@fti.edu.al>
2021-06-02 19:49:46 +0200

    Initial commit

---
+++ src/main/java/al/edu/fti/softwareengineering/universityappfe/universityappfe/UniversityappFeApplication.java
@@ -0,0 +1,13 @@
+package al.edu.fti.softwareengineering.universityappfe.universityappfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityappFeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityappFeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/al/edu/fti/softwareengineering/universityappfe/universityappfe/UniversityappFeApplicationTests.java
@@ -0,0 +1,13 @@
+package al.edu.fti.softwareengineering.universityappfe.universityappfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityappFeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
